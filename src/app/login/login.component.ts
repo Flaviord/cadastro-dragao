cabf4f79ddf4dd5e71722fa154f797b192940a60
Flavio <flavio@ivedha.com>
2019-02-10 13:43:20 -0200

    validation login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  user: string;
-  password: string;
-  constructor() { }
+ 
+  public loginForm: FormGroup;
+  public submitted: boolean = false;
+
+  constructor(private loginService: LoginService,
+             private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  private createForm(): void {
+    this.loginForm = this.fb.group({
+			'user': ['', Validators.required],
+			'password': ['', Validators.required]
+    });
+  }
+
+  get f() { return this.loginForm.controls; }
+
+
+  public onSubmit(): void {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.loginService.login(this.loginForm.controls['user'].value, this.loginForm.controls['password'].value);
   }
 
 }
